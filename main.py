9cd0eaa1ec34b63328fd1b4856b6512dae65c809
C-Zong <chenyangzong621@gmail.com>
2026-03-17 15:44:09 -0400

    chore: add .gitignore

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ from PySide6.QtWidgets import QApplication
 from app.main_window import MainWindow
 import sys
 
-
 def main() -> None:
+    """
+    Entry point of the application.
+
+    Initialize the Qt application, create the main window,
+    and start the event loop.
+    """
+
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
-
 
 if __name__ == "__main__":
     main()
